21ce7b13846c7ceadc0ca18ddf7173bb33261d33
gaborbenko <benkogabor@msn.com>
2016-10-21 19:21:29 +0200

    addedd edge cases test, but still works with expected

--- tests/duedatecalculator.spec.js
+++ tests/duedatecalculator.spec.js
@@ -74,5 +74,31 @@ describe('DueDateCalculator tests', function() {
             expect(dueDate.getFullYear()).to.equal(expectedDueDate.getFullYear());
             dueDateCalculator.setAllowWorkOnSaturday(false);
         });
+
+        it('should complete on next day same time when task comes in the middle of day', function() {
+            var fromDate = new Date(2016, 9, 20, 12);
+            var timearound = timeroundBuilder.days(1).build();
+            var expectedDueDate = new Date(2016, 9, 21, 12, 0 , 0, 0);
+
+            var dueDate = dueDateCalculator.calculateDueDate(fromDate, timearound);
+
+            expect(dueDate.getHours()).to.equal(expectedDueDate.getHours());
+            expect(dueDate.getDate()).to.equal(expectedDueDate.getDate());
+            expect(dueDate.getMonth()).to.equal(expectedDueDate.getMonth());
+            expect(dueDate.getFullYear()).to.equal(expectedDueDate.getFullYear());
+        });
+
+        it('should  complete on next day 3 hours later when the timearound is 1d3h', function() {
+            var fromDate = new Date(2016, 9, 20, 12);
+            var timearound = timeroundBuilder.days(1).hours(3).build();
+            var expectedDueDate = new Date(2016, 9, 21, 15, 0 , 0, 0);
+
+            var dueDate = dueDateCalculator.calculateDueDate(fromDate, timearound);
+
+            expect(dueDate.getHours()).to.equal(expectedDueDate.getHours());
+            expect(dueDate.getDate()).to.equal(expectedDueDate.getDate());
+            expect(dueDate.getMonth()).to.equal(expectedDueDate.getMonth());
+            expect(dueDate.getFullYear()).to.equal(expectedDueDate.getFullYear());
+        });
     });
 });
